1ace72a1a47aab8d48d7bb00474bc054fb7bf69e
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-04-15 10:04:07 +0200

    Passage de code en objet

---
+++ arreraInfo.py
@@ -0,0 +1,249 @@
+from tkinter import*
+import webbrowser
+from ModuleInternet import TestInternet
+import requests
+import geocoder
+from time import*
+from PIL import Image, ImageTk
+
+
+class PArreraInfo :
+    def __init__(self) -> None:
+        #Constante
+        self.__color = "white"
+        self.__textColor = "black"
+        #Var api 
+        self.keyMeteo="ecffd157b2cc9eacbd0d35a45c3dc047"
+        self.urlMeteo="https://api.openweathermap.org/data/2.5/weather?"
+        self.urlGeoLoc = "http://api.ipstack.com/check"
+        self.keyGeoLoc = "b8f00cfb49bfdaf40a317f98314ddc63"
+        self.textTemperature = "Temperature: "
+        self.textHumiditer = "Taux d'humidité : "
+        self.urlNew = "https://newsapi.org/v2/top-headlines?sources=google-news-fr"
+        self.keyNew = "3b43e18afcf945888748071d177b8513"
+        self.nombrePage = "4"
+        #Fenetre tkinter
+        self.__screen = Tk()
+        self.__screen.title("Arrera Info")
+        self.__screen.minsize(600,750)
+        self.__screen.maxsize(600,750)
+        self.__screen.config(bg=self.__color)
+        self.__screen.iconphoto(False,PhotoImage(file="image/icon.png"))
+        #Menu
+        self.__menu = Menu(self.__screen)
+        self.__menu.add_command(label="A propos",command=self.Apropop)
+        self.__screen.configure(menu=self.__menu)
+        #Var image
+        iconParametre = PhotoImage(file="image/iconParametre.png")
+        iconActulisation = PhotoImage(file="image/iconActualisation.png")
+        #Definition des cadre
+        self.cadreMeteoLoc = Frame(self.__screen,bg=self.__color,width=250,height=240)
+        self.cadreMeteoDomicile = Frame(self.__screen,bg=self.__color,width=250,height=240)
+        self.cadreCentral = Frame(self.__screen,bg=self.__color,width=550,height=355)
+        #MeteoLoc
+        self.labelInfoLoc = Label(self.cadreMeteoLoc,text="A votre localisation",bg=self.__color,fg=self.__textColor,font=("arial","15"))
+        self.labelTemperatureLoc = Label(self.cadreMeteoLoc,text=self.textTemperature,bg=self.__color,fg=self.__textColor,font=("arial","15"))
+        self.labelTempLoc = Label(self.cadreMeteoLoc)
+        self.labelHumiditerLoc = Label(self.cadreMeteoLoc,text=self.textHumiditer,bg=self.__color,fg=self.__textColor,font=("arial","15"))
+        #Meteo Domicile
+        self.labelInfoDomicile = Label(self.cadreMeteoDomicile,text="Chez vous",bg=self.__color,fg=self.__textColor,font=("arial","15"))
+        self.labelTemperatureDomicile = Label(self.cadreMeteoDomicile,text=self.textTemperature,bg=self.__color,fg=self.__textColor,font=("arial","15"))
+        self.labelTempDomicile = Label(self.cadreMeteoDomicile)
+        self.labelHumiditerDomicile = Label(self.cadreMeteoDomicile,text=self.textHumiditer,bg=self.__color,fg=self.__textColor,font=("arial","15"))
+        #Cadre central
+        self.boutonActu1 = Button(self.cadreCentral,bg=self.__color,fg=self.__textColor,font=("arial","13"))
+        self.boutonActu2 = Button(self.cadreCentral,bg=self.__color,fg=self.__textColor,font=("arial","13"))
+        self.boutonActu3 = Button(self.cadreCentral,bg=self.__color,fg=self.__textColor,font=("arial","13"))
+        self.boutonActu4 = Button(self.cadreCentral,bg=self.__color,fg=self.__textColor,font=("arial","13"))
+
+        self.labelInternet = Label(self.__screen,text="Internet n'est pas\nDisponible",bg=self.__color,fg=self.__textColor,font=("arial","25"))
+        self.boutonActualisation = Button(self.__screen,image=iconActulisation,bg=self.__color,command=Widget)
+        self.boutonPara = Button(self.__screen,image=iconParametre,bg=self.__color,command=self.Parametre)
+        #affichage
+        self.boutonPara.place(x="0",y="690")
+        self.boutonActualisation.place(x="545",y="690")
+        self.cadreMeteoLoc.pack(side="left",anchor="n")
+        self.cadreMeteoDomicile.pack(side="right",anchor="n")
+        self.cadreCentral.place(relx=.5, rely=.5, anchor="n")
+
+        #Cadre meteo Loc
+        self.labelInfoLoc.place(x="0",y="0")
+        self.labelTemperatureLoc.place(x="0",y="35")
+        self.labelTempLoc.place(relx=.5, rely=.5, anchor="center") 
+        self.labelHumiditerLoc.place(x="0",y="210")
+        #Cadre meteo Domicile
+        self.labelInfoDomicile.place(x="0",y="0")
+        self.labelTemperatureDomicile.place(x="0",y="35")
+        self.labelTempDomicile.place(relx=.5, rely=.5, anchor="center") 
+        self.labelHumiditerDomicile.place(x="0",y="210")
+        #Cadre central
+        self.boutonActu1.place(x="3",y="5")
+        self.boutonActu2.place(x="3",y="65")
+        self.boutonActu3.place(x="3",y="125")
+        self.boutonActu4.place(x="3",y="185")
+        self.Widget()
+        self.__screen.mainloop()
+
+    def Widget(self):
+        etatInternet = TestInternet()
+        if etatInternet == True:
+            self.labelInternet.place_forget()
+            self.cadreMeteoLoc.pack(side="left",anchor="n")
+            self.cadreMeteoDomicile.pack(side="right",anchor="n")
+            self.cadreCentral.place(relx=.5, rely=.5, anchor="n")
+            temperatureLoc , humiditerLoc , descriptionLoc = self.MeteoLoc()
+            temperatureDomicile ,humiditerDomicile , descriptionDomicile = self.MeteoDomicile()
+            url1,titre1Part1,titre1Part2,url2,titre2Part1,titre2Part2,url3,titre3Part1,titre3Part2,url4,titre4Part1,titre4Part2 = self.Actu()
+            iconDomicile = PhotoImage(file=descriptionDomicile)
+            iconLoc = PhotoImage(file=descriptionLoc)
+            self.labelTempDomicile.image_names = iconDomicile
+            self.labelTempLoc.image_names = iconLoc
+            self.labelTempLoc.config(image = iconLoc ,bg=self.__color )
+            self.labelTempDomicile.config(image=iconDomicile,bg=self.__color)
+            self.labelTemperatureLoc.config(text=self.textTemperature+temperatureLoc+" °C")
+            self.labelHumiditerLoc.config(text=self.textHumiditer+humiditerLoc+" %")
+            self.labelHumiditerDomicile.config(text=self.textHumiditer+humiditerDomicile+" %")
+            self.labelTemperatureDomicile.config(text=self.textTemperature+temperatureDomicile+" °C")
+            def Actu1():
+                webbrowser.open(url1)
+            def Actu2():
+                webbrowser.open(url2)
+            def Actu3():
+                webbrowser.open(url3)
+            def Actu4():
+                webbrowser.open(url4)
+            self.boutonActu1.config(text=titre1Part1+"\n"+titre1Part2,command=Actu1)
+            self.boutonActu2.config(text=titre2Part1+"\n"+titre2Part2,command=Actu2)
+            self.boutonActu3.config(text=titre3Part1+"\n"+titre3Part2,command=Actu3)
+            self.boutonActu4.config(text=titre4Part1+"\n"+titre4Part2,command=Actu4)
+        else :
+            self.cadreMeteoLoc.pack_forget()
+            self.cadreMeteoDomicile.pack_forget()
+            self.cadreCentral.place_forget()
+            self.labelInternet.place(relx=.5, rely=.5, anchor="center")
+    
+    def Ecriture(self,file,text):#Fonction d'écriture sur un fichier texte
+        doc = open(file,"w")
+        doc.truncate()
+        doc.write(text)
+        doc.close()
+        return text,file
+    
+    def Lecture(self,file):#Fonction de lecture d'un fichier texte et stokage dans une varriable
+        fichier = open(file,"r")
+        contenu= fichier.readlines()[0]
+        fichier.close()
+        return contenu
+    
+    def FoncModif(self,file):
+        Contenu = self.Lecture(file)
+        ScreenModif = Toplevel()
+        ScreenModif.maxsize(300,150)
+        ScreenModif.minsize(300,150)
+        ScreenModif.wait_visibility(ScreenModif)
+        ScreenModif.wm_attributes('-alpha',0.9)
+        ScreenModif.config(bg=self.color)
+        LabelContenu = Label(ScreenModif,text=Contenu,font=("arial","20"),bg=self.color,fg=self.textColor).pack()
+        entry = Entry(ScreenModif)
+        def Modif():
+            Var = str(entry.get())
+            self.Ecriture(file,Var)
+            ScreenModif.destroy()
+        modif = Button(ScreenModif,text="Modifier",bg=self.color,fg=self.textColor,command=Modif).pack(side="right",anchor="s")
+        entry.pack(side="left",anchor="s")
+    
+    def Parametre(self):
+        screenPara = Toplevel()
+        def Ville():
+            self.FoncModif("config/ville.txt")
+        screenPara.title("Arrera Info")
+        screenPara.minsize(200,100)
+        screenPara.maxsize(200,100)
+        screenPara.config(bg=self.color)
+        boutonVille = Button(screenPara,text="Localisation domicile",bg=self.color,fg=self.textColor,font=("arial","15"),command=Ville).pack(side="left") 
+    
+    def Geoloc(self):
+        myPublic_IP = requests.get("http://wtfismyip.com/text").text.strip()
+        ip = geocoder.ip(myPublic_IP)
+        loc = ip.latlng
+        lat = str(loc[0])
+        long = str(loc[1])
+        return lat , long
+    
+    def MeteoLoc(self):
+        lat , long  = self.Geoloc()
+        ReponseTemp = requests.get(self.urlMeteo+"appid="+self.keyMeteo+"&lat="+lat+"&lon="+long+"&lang=fr"+"&units=metric").json()
+        if ReponseTemp['cod'] == 404 :
+            ville = input("Entrer votre ville : ")
+            ReponseTemp = requests.get(self.urlMeteo+"appid="+self.keyMeteo+"&q="+ville+"&lang=fr"+"&units=metric").json()
+            return "none" ,"none","none"
+        else :
+            temperature = str(ReponseTemp['main']['temp']) 
+            humiditer = str(ReponseTemp['main']['humidity'])
+            code = ReponseTemp['weather'][0]['icon'] 
+            icon = "weather/"+code+".png"
+        return temperature ,humiditer,icon
+    
+    def MeteoDomicile(self):
+        ville = self.Lecture("config/ville.txt")
+        ReponseTemp = requests.get(self.urlMeteo+"appid="+self.keyMeteo+"&q="+ville+"&lang=fr"+"&units=metric").json()
+        if ReponseTemp['cod'] == 404 :
+            ville = input("Entrer votre ville : ")
+            ReponseTemp = requests.get(self.urlMeteo+"appid="+self.keyMeteo+"&q="+ville+"&lang=fr"+"&units=metric").json()
+            return "none" ,"none","none"
+        else :
+            temperature = str(ReponseTemp['main']['temp']) 
+            humiditer = str(ReponseTemp['main']['humidity'])
+            code = ReponseTemp['weather'][0]['icon'] 
+            icon = "weather/"+code+".png"
+        return temperature ,humiditer,icon
+    
+    def Netoyage(self,dictionnnaire):
+        url= dictionnnaire["url"]
+        titre = dictionnnaire["title"]
+        return url,titre
+    
+    def Actu(self):
+        CompleteURLNew = self.urlNew+"&pageSize="+self.nombrePage+"&apiKey="+self.keyNew
+        article = requests.get(CompleteURLNew).json()["articles"]
+        url1,titreFull1 = self.Netoyage(article[0])
+        url2,titreFull2 = self.Netoyage(article[1])
+        url3,titreFull3 = self.Netoyage(article[2])
+        url4,titreFull4 = self.Netoyage(article[3])
+        titre1Part1 = titreFull1[:len(titreFull1)//2]
+        titre1Part2 = titreFull1[len(titreFull1)//2:]
+        titre2Part1 = titreFull2[:len(titreFull2)//2]
+        titre2Part2 = titreFull2[len(titreFull2)//2:]
+        titre3Part1 = titreFull3[:len(titreFull3)//2]
+        titre3Part2 = titreFull3[len(titreFull3)//2:]
+        titre4Part1 = titreFull4[:len(titreFull4)//2]
+        titre4Part2 = titreFull4[len(titreFull4)//2:]
+        return url1,titre1Part1,titre1Part2,url2,titre2Part1,titre2Part2,url3,titre3Part1,titre3Part2,url4,titre4Part1,titre4Part2
+
+    def Apropop(self):
+        #Variable
+        nameApp = "Arrera Info"
+        versionApp = "I2024-"
+        imagePath = "image/icon.png"
+        copyrightApp = "Copyright Arrera Software by Baptiste P 2023-2024"
+        tailleIMG = (100,100)
+        #Creation de la fenetre
+        about = Toplevel()
+        about.title("A propos :"+nameApp)
+        about.maxsize(400,300)
+        about.minsize(400,300)
+        #Traitement Image
+        imageOrigine = Image.open(imagePath)    
+        imageRedim = imageOrigine.resize(tailleIMG)
+        icon = ImageTk.PhotoImage(imageRedim)
+        #Label
+        labelIcon = Label(about,image=icon)
+        labelName = Label(about,text="\n"+nameApp+"\n",font=("arial","12"))
+        labelVersion = Label(about,text=versionApp+"\n",font=("arial","11"))
+        labelCopyright = Label(about,text=copyrightApp,font=("arial","9"))
+        #affichage
+        labelIcon.pack()
+        labelName.pack()
+        labelVersion.pack()
+        labelCopyright.pack()
+        about.mainloop()
